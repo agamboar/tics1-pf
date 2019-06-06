c7d526bc889f0026492c17fc32bc34080a29a56c
agamboar <gamboaalvaro96@gmail.com>
2019-06-06 00:42:08 -0400

    blabla

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    return app
 
-@app.route("/")
 
-def hello():
-    return render_template('home.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
