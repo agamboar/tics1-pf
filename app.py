a41048c12b19cfd921817851cf9b585355f06c02
agamboar <gamboaalvaro96@gmail.com>
2019-06-06 01:23:14 -0400

    typo

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import render_template, Blueprint
+from flask import Flask, render_template
 
-app = Blueprint('main', __name__)
+app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 
 def index():
     return render_template('index.html')
 
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
